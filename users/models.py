2233d39c5373fe7f6d72b6b2d5ed8288216099be
msalah593 <you@example.com>
2019-02-13 22:00:28 +0200

    cleanup

--- users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
-class CustomUser(AbstractUser):
-    email = models.EmailField(('email address'), blank=False)
-    def __str__(self):
-        return self.email
