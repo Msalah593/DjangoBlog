30a7f68b8a5843d1e1aec3be4361007b3d88b586
msalah593 <you@example.com>
2019-02-21 15:10:07 +0200

    add search bar for articles

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,reverse
 from django.views import generic
 from .models import Article
 from django.urls import reverse_lazy
+from django.utils import six
+from django.db.models import Q
 
 # Create your views here.
 
@@ -10,6 +12,42 @@ class ArticleList(generic.ListView):
     model=Article
     template_name='index.html'
     context_object_name='articles'
+
+    def get_queryset(self):
+        """
+        Return the list of items for this view.
+
+        The return value must be an iterable and may be an instance of
+        `QuerySet` in which case `QuerySet` specific behavior will be enabled.
+        """
+        query = self.request.GET.get('q')
+        if self.queryset is not None:
+            queryset = self.queryset
+            if isinstance(queryset, QuerySet):
+                queryset = queryset.all()
+        elif self.model is not None:
+            queryset = self.model._default_manager.all()
+        else:
+            raise ImproperlyConfigured(
+                "%(cls)s is missing a QuerySet. Define "
+                "%(cls)s.model, %(cls)s.queryset, or override "
+                "%(cls)s.get_queryset()." % {
+                    'cls': self.__class__.__name__
+                }
+            )
+        if query:
+            queryset = queryset.filter(Q(title__icontains=query) |
+                Q(body__icontains=query) |
+                Q(author__email__icontains=query) | 
+                Q(author__username__icontains=query))
+        ordering = self.get_ordering()
+        if ordering:
+            if isinstance(ordering, six.string_types):
+                ordering = (ordering,)
+            queryset = queryset.order_by(*ordering)
+
+        return queryset
+
 
 class ArticleCreate(generic.CreateView):
     model=Article
