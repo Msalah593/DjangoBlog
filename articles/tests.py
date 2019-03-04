47b6b1dfbadaba0dd93d12b6e130e1dbfa5e4bc4
Mohamed Salah <msalah593@gmail.com>
2019-03-04 02:33:19 +0200

    updae article fixtures

--- articles/tests.py
+++ articles/tests.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 from users.models import CustomUser
 
 class TestArticleUpdateView(TestCase):
-    fixtures=['dumb']
+    fixtures=['initial_data']
     def test_update_article_pass(self):
         self.client.login(username='menbawy',password='men@1234')
         url=reverse('article-update',kwargs={'pk':14})
@@ -19,13 +19,13 @@ class TestArticleUpdateView(TestCase):
                                     {'title':'good','body':'good'})
         self.assertEqual(Article.objects.get(id=12).title,'good')
 class TestArticleListView(TestCase):
-    fixtures=['dumb']
+    fixtures=['initial_data']
     def test_search(self):
         url = "%s?q=test" % reverse('home')
         response = self.client.get(url)
         self.assertEqual(response.context['articles'].count(),5)
 class TestArticleCreateView(TestCase):
-    fixtures=['dumb']
+    fixtures=['initial_data']
     def test_create(self):
         self.client.login(username='admin',password='admin@1234')
         count=Article.objects.count()
@@ -34,7 +34,7 @@ class TestArticleCreateView(TestCase):
         self.assertEqual(Article.objects.count(),count+1)
         self.assertEqual(response.status_code,302)    
 class TestSmokeTests(TestCase):
-    fixtures=['dumb']
+    fixtures=['initial_data']
     def test_home(self):
         url=reverse('home')
         response=self.client.get(url)
